dc8b1b108ae8b5170de74b87c2da165a64f1e6ee
swood456 <smwood456@gmail.com>
2019-01-15 23:23:28 -0500

    added simple input manager for horizontal and vertical axes

---
+++ TestMonoGame/InputManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+
+namespace TestMonoGame
+{
+    class InputManager
+    {
+        private float horizontalInput;
+        private float verticalInput;
+
+        public InputManager()
+        {
+            KeyboardState kState = Keyboard.GetState();
+
+            horizontalInput = 0f;
+            verticalInput = 0f;
+
+            if (kState.IsKeyDown(Keys.Up) || kState.IsKeyDown(Keys.W))
+            {
+                verticalInput += 1.0f;
+            }
+
+            if (kState.IsKeyDown(Keys.Down) || kState.IsKeyDown(Keys.S))
+            {
+                verticalInput -= 1.0f;
+            }
+
+            if (kState.IsKeyDown(Keys.Right) || kState.IsKeyDown(Keys.D))
+            {
+                horizontalInput += 1.0f;
+            }
+
+            if (kState.IsKeyDown(Keys.Left) || kState.IsKeyDown(Keys.A))
+            {
+                horizontalInput -= 1.0f;
+            }
+        }
+
+        public float HorizontalInput { get => horizontalInput; set => horizontalInput = value; }
+        public float VerticalInput { get => verticalInput; set => verticalInput = value; }
+    }
+}
